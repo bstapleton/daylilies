2ffdebce2aebe2153f278d67c1da424e6a7e679e
Ben Stapleton <ben.stapleton@barbon.com>
2019-05-18 08:58:47 +0100

    Added links to view the individual plant page

--- resources/views/plants-list.blade.php
+++ resources/views/plants-list.blade.php
@@ -12,7 +12,7 @@
                 <article class="h-flex" itemscope itemtype="http://schema.org/Product">
                     <link itemprop="additionalType" href="http://www.productontology.org/id/Plant" />
                     <div class="c-plant-list__thumbnail">
-                        <img class="c-plant-list__image" itemprop="image" src="{{asset('images/thumbnails/thumb-' . $plant->slug . '.jpg')}}" alt="{{$plant->name}}" />
+                        <a href="{{ URL::route('view-plant', $plant->slug) }}" title="View details for {{ $plant->name }}"><img class="c-plant-list__image" itemprop="image" src="{{asset('images/thumbnails/thumb-' . $plant->slug . '.jpg')}}" alt="{{$plant->name}}" /></a>
                     </div>
                     <div class="c-plant-list__detail">
                         <h2 itemprop="name" class="c-plant-list__title">{{$plant->name}}</h2>
@@ -80,7 +80,7 @@
                                         <link itemprop="availability" href="http://schema.org/OutOfStock" />Out of stock
                                     @endif
                                 </p>
-                                <a href="#" class="c-button c-button--default">View Details</a>
+                                <a href="{{ URL::route('view-plant', $plant->slug) }}" title="View details for {{ $plant->name }}" class="c-button c-button--default">View Details</a>
                                 @if ($plant->in_stock == true)
                                     <a href="#" class="c-button c-button--success">Add to order</a>
                                 @endif
