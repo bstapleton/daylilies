6de4d71cf61d4ec69386aa4157f109debac610de
Ben Stapleton <ben.stapleton@barbon.com>
2019-05-18 08:56:28 +0100

    Using named routes to access them easier through blade templates

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,21 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/contact', function () {
     return view('contact-us');
-});
+})->name('contact-us');
 
 Route::get('/order-form', function () {
     return view('order-form');
-});
+})->name('order-form');
 
-Route::get('/plants', 'PlantController@index');
-    Route::get('/plants/new', 'PlantController@listNew');
-    Route::get('/plants/category/{category}', 'PlantController@listCategory');
-    Route::get('/plants/foliage/{foliage}', 'PlantController@listFoliage');
-    Route::get('/plants/season/{season}', 'PlantController@listSeason');
-    Route::get('/plants/view/{name}', 'PlantController@view');
+Route::get('/plants', 'PlantController@index')->name('plants');
+    Route::get('/plants/new', 'PlantController@listNew')->name('new');
+    Route::get('/plants/category/{category}', 'PlantController@listCategory')->name('category');
+    Route::get('/plants/foliage/{foliage}', 'PlantController@listFoliage')->name('foliage');
+    Route::get('/plants/season/{season}', 'PlantController@listSeason')->name('season');
+    Route::get('/plants/view/{slug}', 'PlantController@view')->name('view-plant');
 
-Route::get('/breeder/{breederSlug}', 'BreederController@listPlants');
-
-Route::get('/upload', 'ImportController@upload'); // localhost:8000/
-Route::post('/uploadFile', 'ImportController@uploadFile');
+Route::get('/breeder/{breederSlug}', 'BreederController@listPlants')->name('breeder');
