fa36b16f6fba81c92d7dee0062da8a9d219300cc
Ben Stapleton <ben.stapleton@barbon.com>
2019-05-21 21:53:05 +0100

    Added todo to fix the breeder route since its borked after normalisation

--- app/Http/Controllers/BreederController.php
+++ app/Http/Controllers/BreederController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Plantss;
+use App\Plant;
 
 /**
  * Class BreederController
@@ -16,16 +16,21 @@ class BreederController extends Controller
      * @param string $breederSlug
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function listPlants(string $breederSlug) {
-        $plants = Plantss::where('breeder_slug', $breederSlug)
+    public function listPlants(string $slug) {
+        // TODO: currently borked after the data normalisation!
+        $plants = Plant::where('slug', $slug)
             ->orderBy('name', 'asc')->get();
 
         if ($plants->count() < 1) {
-            return view('error', ['category' => 'Breeder Request', 'request' => $breederSlug]);
+            return view('error', ['category' => 'Breeder Request', 'request' => $slug]);
         }
 
         $humanFriendlyBreederName = $plants->first()->breeder;
 
-        return view('plants-list', ['plants' => $plants, 'category' => $humanFriendlyBreederName, 'title' => 'Daylilies by ' . $humanFriendlyBreederName]);
+        return view('plants-list', [
+            'plants' => $plants,
+            'category' => $humanFriendlyBreederName,
+            'title' => 'Daylilies by ' . $humanFriendlyBreederName
+        ]);
     }
 }
