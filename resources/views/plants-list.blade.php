3264427abf4911cdc98441712c110ebabdc28899
Ben Stapleton <ben.stapleton@barbon.com>
2019-06-01 19:33:51 +0100

    Fixed navigation styles a little

--- resources/views/plants-list.blade.php
+++ resources/views/plants-list.blade.php
@@ -6,17 +6,22 @@
 
 @section('content')
     <h1 class="h-heading l-page-content__title">
-    @isset($categoryTitle)
-        {{ $categoryTitle }}
+    @if($isCategoryView)
+        {{ $plants->first()->category->first()->name }} daylilies
     @else
         Daylily listings
-    @endisset
-    </h1>
-    @isset($category)
-    @if ($category == 'large')
-        @include('partials/introduction_large')
     @endif
-    @endisset
+    </h1>
+    @if($isCategoryView)
+        <div class="c-card c-card--light">
+        {!! $plants->first()->category()->first()->description !!}
+        </div>
+    @endif
+    <div class="c-card c-card--light">
+        <p>To view more details and a larger image of each plant, please click on the thumbnail, or the 'View details' buttons.</p>
+        <p>It should be noted that entries on the website are only those for which we currently have photographs. If you can't find what you are after, please make an enquiry by going to the <a href="{{ URL::route('contact-us') }}">Contact Us</a> page.</p>
+        <p>All photographs on this website were taken in our garden and accurately show how daylilies grow in an english climate. Please do not reuse these images without permission.</p>
+    </div>
     <ul class="c-plant-list">
         @foreach($plants as $plant)
             <li class="h-list--unstyled c-plant-list__item">
