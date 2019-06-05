1c644d457cf3d56185c098c137705b24e01e09c5
Ben Stapleton <ben.stapleton@barbon.com>
2019-06-05 21:57:26 +0100

    Make use of a partial to limit repetition in blade templates
    There's probably more I can do on this side of things, so I'll likely revisit it again later on in development.

---
+++ resources/views/partials/plants-introduction.blade.php
@@ -0,0 +1,26 @@
+<h1 class="h-heading l-page-content__title">
+    @if($isCategoryView)
+        {{ $plants->first()->category->first()->name }} daylilies
+    @elseif(isset($pageHeading))
+        {{ $pageHeading }}
+    @else
+        Daylily listings
+    @endif
+</h1>
+@if($isCategoryView)
+    <div class="c-card c-card--light">
+        {!! $plants->first()->category()->first()->description !!}
+    </div>
+@endif
+<div class="c-card c-card--light">
+    <p>To view more details and a larger image of each plant, please click on the thumbnail, or the 'View details' buttons.</p>
+    <p>It should be noted that entries on the website are only those for which we currently have photographs. If you can't find what you are after, please make an enquiry by going to the <a href="{{ URL::route('contact-us') }}">Contact Us</a> page.</p>
+    <p>All photographs on this website were taken in our garden and accurately show how daylilies grow in an english climate. Please do not reuse these images without permission.</p>
+</div>
+<div class="c-plant-view-controls">
+    <ul class="h-list--horizontal h-list--unstyled h-flex--align-center h-flex--justify-right">
+        <li class="h-list--horizontal__item">View as:</li>
+        <li class="h-list--horizontal__item"><button class="c-button--default">List</button></li>
+        <li class="h-list--horizontal__item"><button class="c-button--default">Grid</button></li>
+    </ul>
+</div>
