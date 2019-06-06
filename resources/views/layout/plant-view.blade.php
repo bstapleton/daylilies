ff5c3415df8d093ff7cbaa7557e21eeb674531b3
Ben Stapleton <ben.stapleton@barbon.com>
2019-06-06 19:25:26 +0100

    Updated footer message on plant view

--- resources/views/layout/plant-view.blade.php
+++ resources/views/layout/plant-view.blade.php
@@ -42,7 +42,8 @@
 
         @section('layout-footer')
             <div class="l-plant-view-footer">
-                default footer here
+                <p>Copyright {{ date('Y') }} A La Carte Daylilies.</p>
+                <p>All of our daylily photos were taken in our garden on the Isle of Wight and are also copyrighted to A La Carte Daylilies. Please do no re-use these images without our express permission to do so. Thank you.</p>
             </div>
         @show
     </div>
