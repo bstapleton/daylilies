182a5921c2df35732ccfa9feedea3c6da53ef1ca
Ben Stapleton <ben.stapleton@barbon.com>
2019-07-21 16:21:03 +0100

    Fixed some rendering issues with the order form print view

--- resources/views/layout/order-form.blade.php
+++ resources/views/layout/order-form.blade.php
@@ -5,7 +5,7 @@
     <title>A La Carte Daylilies - Order form</title>
     <meta name="theme-color" content="#006600">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link href="{{ asset('css/print.css') }}" rel="stylesheet" media="print">
+    <link href="{{ asset('css/print.css') }}" rel="stylesheet" media="all">
 </head>
 <body class="l-orderForm">
 @section('content')
