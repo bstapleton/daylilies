b9b8cd23ce8d1c36b82907f29bbffc6ac01ca06b
Ben Stapleton <ben.stapleton@barbon.com>
2020-03-09 19:28:13 +0000

    Run a compression for the production variant of the css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 let gulp = require("gulp");
 let sass = require("gulp-sass");
+let clean = require("gulp-clean-css");
 let inputPath = "resources/sass";
 let outputPath = "public/css";
 
@@ -7,7 +8,11 @@ gulp.task("default", function(done) {
     console.log("Generating CSS files...");
     gulp.src(inputPath + "/app.scss")
         .pipe(sass().on("error", sass.logError))
-        .pipe(gulp.dest(outputPath));
+        .pipe(gulp.dest(outputPath))
+        .pipe(clean({debug: true}, (details) => {
+            console.log(`${details.name} : ${details.stats.originalSize} => ${details.stats.minifiedSize}`);
+        }))
+        .pipe(gulp.dest(`${outputPath}/dist`));
     gulp.src(inputPath + "/print.scss")
         .pipe(sass().on("error", sass.logError))
         .pipe(gulp.dest(outputPath));
