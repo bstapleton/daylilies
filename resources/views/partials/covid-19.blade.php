5cf3f30d66be61af6b1f6e95e4610e9b40acd58a
Ben Stapleton <ben.stapleton@barbon.com>
2020-03-30 17:53:32 +0100

    covid-19 static message

---
+++ resources/views/partials/covid-19.blade.php
@@ -0,0 +1,3 @@
+<div class="c-card c-card--danger">
+    <p>We regret that, due to the COVID-19 outbreak, we have suspended our mail order operation for the time being. Please <a href="{{ URL::route('contact-us') }}">contact us</a> before ordering.</p>
+</div>
